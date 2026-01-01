f8e1d48517d591b6340c73ef9f621c8cfd9faf0c
Shivam Yadav <shivamptu4566@gmail.com>
2026-01-01 12:25:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:3000"||"https://stalwart-mousse-b8bcbc.netlify.app"
+  origin: "http://localhost:3000"||("https://stalwart-mousse-b8bcbc.netlify.app/")
   credentials: true,
 };
 app.use(cors(corsOption));
