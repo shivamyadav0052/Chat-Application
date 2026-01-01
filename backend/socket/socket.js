dd80d45a569791c4f1ed071e28279e4d8ef22fae
Shivam Yadav <shivamptu4566@gmail.com>
2026-01-01 12:37:30 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,11 +6,16 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors:{
-        origin:['http://localhost:3000'],
-        methods:['GET', 'POST'],
-    },
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "https://stalwart-mousse-b8bcbc.netlify.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
+
 
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
